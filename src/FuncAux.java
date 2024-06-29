e477258bc987ad421478113810f8279995b13397
JulioCesar-A <juliocesar02e@gmail.com>
2024-06-29 19:33:08 -0300

    Criação Base Menu Inicial

---
+++ src/FuncAux.java
@@ -0,0 +1,18 @@
+public abstract class FuncAux {
+    
+    public static void menuIni() {
+        System.out.println("Bem-vindos (as) ao sistema de criação e preenchimento de planilhas");
+        System.out.println();
+        System.out.println("Selecione uma das opções");
+        System.out.println(" ------------------------");
+        System.out.println("| 1. Criar arquivo");
+        System.out.println("| 2. Preencher planilha");
+        System.out.println("| 3. Excluir planilha");
+        System.out.println("| 4. Guia de uso");
+        System.out.println(" ------------------------");
+        System.out.println();
+        System.out.println("Recomendamos que selecione a opção Guia de Uso antes de fazer quaisquer operações no sistema");
+    }
+    
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        FuncAux.menuIni();
     }
+
+
 }
